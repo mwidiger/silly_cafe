0a69dc77978533bc563eb11715e28d55f1ffb4d5
Matt Widiger <matt.widiger@gmail.com>
2023-04-10 09:30:55 -0400

    Create seed for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Item.delete_all
+Price.delete_all
+TaxRate.delete_all
+Order.delete_all
+OrderLineItem.delete_all
+Discount.delete_all
+OrderDiscount.delete_all
+
+i = Item.create(name: "Soda")
+Price.create(item: i, price: '3.00')
+TaxRate.create(item: i, tax_rate: '0.05')
+
+ii = Item.create(name: "Pizza")
+Price.create(item: i, price: '2.50')
+TaxRate.create(item: i, tax_rate: '0.08')
+
+iii = Item.create(name: "Fries")
+Price.create(item: i, price: '2.00')
+TaxRate.create(item: i, tax_rate: '0.05')
+
+d = Discount.create(item: i, when_ordered_item: ii, discount_rate: '0.25')
