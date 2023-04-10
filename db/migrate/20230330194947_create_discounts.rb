0711f09d927a47d0e16df216fec72cb8bf4c6064
Matt Widiger <matt.widiger@gmail.com>
2023-04-10 09:30:02 -0400

    Fixed discount db

--- db/migrate/20230330194947_create_discounts.rb
+++ db/migrate/20230330194947_create_discounts.rb
@@ -2,10 +2,10 @@ class CreateDiscounts < ActiveRecord::Migration[7.0]
   def change
     create_table :discounts do |t|
       t.string :name
-      t.string :item_id, index: true
+      t.integer :item_id, index: true
 
       t.float :discount_rate
-      t.string :when_ordered_item_id, index: true
+      t.integer :when_ordered_item_id, index: true
 
       t.timestamps
     end
